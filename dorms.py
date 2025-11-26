dbf41063b9c0e9bb49289756aa14a1acfdcb0f08
ShayShN <shayshn1234@gmail.com>
2025-11-26 13:02:00 +0200

    break

---
+++ dorms.py
@@ -0,0 +1,11 @@
+class DormA:
+    def __init__(self):
+        self.dorm_a = []
+        
+class DormB:
+    def __init__(self):
+        self.dorm_b = []
+        
+class StantingList:
+    def __init__(self):
+        self.stand_list = []
